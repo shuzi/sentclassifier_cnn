b8a2ccac04611b3c6a445d2db262bb3920d77917
Minwei Feng <mfeng@us.ibm.com>
2016-02-01 12:08:44 -0500

    add code for cnn based sent classifier

---
+++ cnn.lua
@@ -0,0 +1,96 @@
+#!/opt/share/torch-7.0/bin/th
+
+cmd = torch.CmdLine('_')
+cmd:text()
+cmd:text('Options:')
+cmd:option('-seed', 1, 'fixed input seed for repeatable experiments')
+cmd:option('-threads', 1, 'number of threads')
+cmd:option('-optimization', 'SGD', 'optimization method: SGD | ASGD | CG | LBFGS')
+cmd:option('-learningRate', 1e-2, 'learning rate at t=0')
+cmd:option('-batchSize', 1, 'mini-batch size (1 = pure stochastic)')
+cmd:option('-batchSizeTest', 1, 'mini-batch size (1 = pure stochastic)')
+cmd:option('-weightDecay', 0, 'weight decay (SGD only)')
+cmd:option('-momentum', 0, 'momentum (SGD only)')
+cmd:option('-t0', 1, 'start averaging at t0 (ASGD only), in nb of epochs')
+cmd:option('-maxIter', 2, 'maximum nb of iterations for CG and LBFGS')
+cmd:option('-type', 'float', 'type: double | float | cuda')
+cmd:option('-trainFile', 'train', 'input training file')
+cmd:option('-validFile', 'valid', 'input validation file')
+cmd:option('-testFile', 'test', 'input test file')
+cmd:option('-embeddingFile', 'embedding', 'input embedding file')
+cmd:option('-embeddingDim', 100, 'input word embedding dimension')
+cmd:option('-contConvWidth', 2, 'continuous convolution filter width')
+cmd:option('-wordHiddenDim', 200, 'first hidden layer output dimension')
+cmd:option('-numFilters', 1000, 'CNN filters amount')
+cmd:option('-hiddenDim', 1000, 'second hidden layer output dimension')
+cmd:option('-numLabels', 311, 'label quantity')
+cmd:option('-epoch', 200, 'maximum epoch')
+cmd:option('-L1reg', 0, 'L1 regularization coefficient')
+cmd:option('-L2reg', 1e-4, 'L2 regularization coefficient')
+cmd:option('-trainMaxLength', 100, 'maximum length for training')
+cmd:option('-testMaxLength', 100, 'maximum length for valid/test')
+cmd:option('-gradClip', 0.5, 'gradient clamp')
+cmd:option('-gpuID', 1, 'GPU ID')
+cmd:text()
+opt = cmd:parse(arg or {})
+print(opt)
+--opt.rundir = cmd:string('experiment', opt, {dir=true})
+--paths.mkdir(opt.rundir)
+--cmd:log(opt.rundir .. '/log', params)
+
+if opt.type == 'float' then
+   print('==> switching to floats')
+   require 'torch'
+   require 'nn'
+   require 'optim'
+   torch.setdefaulttensortype('torch.FloatTensor')
+elseif opt.type == 'cuda' then
+   print('==> switching to CUDA')
+   require 'cutorch'
+   require 'cunn'
+   require 'optim'
+   torch.setdefaulttensortype('torch.FloatTensor')
+   cutorch.setDevice(opt.gpuID)
+   print('GPU DEVICE ID = ' .. cutorch.getDevice())
+end
+print("####")
+print("default tensor type"  .. torch.getdefaulttensortype())
+torch.setnumthreads(opt.threads)
+torch.manualSeed(opt.seed)
+math.randomseed(opt.seed)
+mapWordIdx2Vector = torch.Tensor()
+mapWordStr2WordIdx = {}
+mapWordIdx2WordStr = {}
+trainDataSet = {}
+validDataSet = {}
+testDataSet = {}
+trainDataTensor = torch.Tensor()
+trainDataTensor_y = torch.Tensor()
+validDataTensor = torch.Tensor()
+validDataTensor_y = {}
+testDataTensor = torch.Tensor()
+testDataTensor_y = {}
+
+
+dofile 'prepareData.lua'
+if opt.type == 'cuda' then
+  trainDataTensor =  trainDataTensor:cuda()
+  trainDataTensor_y =  trainDataTensor_y:cuda()
+  validDataTensor = validDataTensor:cuda()
+  testDataTensor = testDataTensor:cuda()
+end
+dofile 'train.lua'
+collectgarbage()
+collectgarbage()
+
+sys.tic()
+epoch = 1
+validState = {}
+testState = {}
+while epoch <= opt.epoch do
+   train()
+   test(validDataTensor, validDataTensor_y, validState)
+   test(testDataTensor, testDataTensor_y, testState)
+   epoch = epoch + 1
+end
+
---
+++ train.lua
@@ -0,0 +1,185 @@
+model = nn.Sequential()
+model:add(nn.LookupTable(mapWordIdx2Vector:size()[1], opt.embeddingDim))
+model:add(nn.View(opt.batchSize*trainDataTensor:size()[2], opt.embeddingDim))
+model:add(nn.Linear(opt.embeddingDim, opt.wordHiddenDim))
+model:add(nn.View(opt.batchSize, trainDataTensor:size()[2], opt.wordHiddenDim))
+model:add(nn.Tanh())
+model:add(nn.TemporalConvolution(opt.wordHiddenDim, opt.numFilters, opt.contConvWidth))
+model:add(nn.Max(2))
+model:add(nn.Tanh())
+model:add(nn.Linear(opt.numFilters, opt.hiddenDim))
+model:add(nn.Tanh())
+model:add(nn.Linear(opt.hiddenDim, opt.numLabels))
+model:add(nn.LogSoftMax())
+criterion = nn.ClassNLLCriterion()
+
+model:get(1).weight:copy(mapWordIdx2Vector)
+
+model_test = nn.Sequential()
+model_test:add(nn.LookupTable(mapWordIdx2Vector:size()[1], opt.embeddingDim))
+model_test:add(nn.View(opt.batchSizeTest*validDataTensor:size()[2], opt.embeddingDim))
+model_test:add(nn.Linear(opt.embeddingDim, opt.wordHiddenDim))
+model_test:add(nn.View(opt.batchSizeTest, validDataTensor:size()[2], opt.wordHiddenDim))
+model_test:add(nn.Tanh())
+model_test:add(nn.TemporalConvolution(opt.wordHiddenDim, opt.numFilters, opt.contConvWidth))
+model_test:add(nn.Max(2))
+model_test:add(nn.Tanh())
+model_test:add(nn.Linear(opt.numFilters, opt.hiddenDim))
+model_test:add(nn.Tanh())
+model_test:add(nn.Linear(opt.hiddenDim, opt.numLabels))
+model_test:add(nn.LogSoftMax())
+
+model_test:get(1).weight = model:get(1).weight
+model_test:get(3).weight = model:get(3).weight
+model_test:get(3).bias = model:get(3).bias
+model_test:get(6).weight = model:get(6).weight
+model_test:get(6).bias = model:get(6).bias
+model_test:get(9).weight = model:get(9).weight
+model_test:get(9).bias = model:get(9).bias
+model_test:get(11).weight = model:get(11).weight
+model_test:get(11).bias = model:get(11).bias
+
+
+if opt.type == 'cuda' then
+   model:cuda()
+   criterion:cuda()
+   model_test:cuda()
+   model_test:get(1).weight = model:get(1).weight
+   model_test:get(3).weight = model:get(3).weight
+   model_test:get(3).bias = model:get(3).bias
+   model_test:get(6).weight = model:get(6).weight
+   model_test:get(6).bias = model:get(6).bias
+   model_test:get(9).weight = model:get(9).weight
+   model_test:get(9).bias = model:get(9).bias
+   model_test:get(11).weight = model:get(11).weight
+   model_test:get(11).bias = model:get(11).bias
+end
+if model then
+   parameters,gradParameters = model:getParameters()
+--   parameters = parameters:contiguous()
+--   gradParameters = gradParameters:contiguous()
+   parametersClone = parameters:clone()
+end
+print(model)
+print(criterion)
+
+if opt.optimization == 'CG' then
+   optimState = {
+      maxIter = opt.maxIter
+   }
+   optimMethod = optim.cg
+
+elseif opt.optimization == 'LBFGS' then
+   optimState = {
+      learningRate = opt.learningRate,
+      maxIter = opt.maxIter,
+      nCorrection = 10
+   }
+   optimMethod = optim.lbfgs
+
+elseif opt.optimization == 'SGD' then
+   optimState = {
+      learningRate = opt.learningRate,
+      weightDecay = opt.weightDecay,
+      momentum = opt.momentum,
+      learningRateDecay = 0
+   }
+   optimMethod = optim.sgd
+
+elseif opt.optimization == 'ASGD' then
+   optimState = {
+      eta0 = opt.learningRate,
+      t0 = trsize * opt.t0
+   }
+   optimMethod = optim.asgd
+
+else
+   error('unknown optimization method')
+end
+
+function train()
+    epoch = epoch or 1
+    local time = sys.clock()
+    model:training()
+    local batches = trainDataTensor:size()[1]/opt.batchSize
+    local bs = opt.batchSize
+    shuffle = torch.randperm(batches)
+    for t = 1,batches,1 do
+        local begin = (shuffle[t] - 1)*bs + 1
+        local input = trainDataTensor:narrow(1, begin , bs) 
+        local target = trainDataTensor_y:narrow(1, begin , bs)
+        
+        local feval = function(x)
+            if x ~= parameters then
+                parameters:copy(x)
+            end
+            gradParameters:zero()
+            local f = 0
+            local output = model:forward(input)
+            f = criterion:forward(output, target)
+            local df_do = criterion:backward(output, target)
+            model:backward(input, df_do) 
+
+            if opt.L1reg ~= 0 then
+               local norm, sign = torch.norm, torch.sign
+               f = f + opt.L1reg * norm(parameters,1)
+               gradParameters:add( sign(parameters):mul(opt.L1reg) )
+            end
+            if opt.L2reg ~= 0 then
+    --           local norm, sign = torch.norm, torch.sign
+    --           f = f + opt.L2reg * norm(parameters,2)^2/2
+               parametersClone:copy(parameters)
+               gradParameters:add( parametersClone:mul(opt.L2reg) )
+            end
+--           gradParameters:clamp(-opt.gradClip, opt.gradClip)
+            return f,gradParameters
+        end
+
+        if optimMethod == optim.asgd then
+            _,_,average = optimMethod(feval, parameters, optimState)
+        else
+--            a,b = model:parameters()
+         --   print('a ' .. a[1][1][1]);
+            optimMethod(feval, parameters, optimState)
+         --   print('  ' .. a[1][1][1]);
+        end
+        model:get(1).weight:narrow(1,1,2):fill(0)
+    end
+
+    time = sys.clock() - time
+    print("\n==> time for 1 epoch = " .. (time) .. ' seconds')
+end
+
+function test(inputDataTensor, inputTarget, state)
+    local time = sys.clock()
+    model_test:evaluate()
+    local bs = opt.batchSizeTest
+    local batches = inputDataTensor:size()[1]/bs
+    local correct = 0
+    for t = 1,batches,1 do
+        local begin = (t - 1)*bs + 1
+        local input = inputDataTensor:narrow(1, begin , bs)
+
+        local pred = model_test:forward(input)
+        local prob, pos = torch.max(pred, 2)
+        for m = 1,bs do
+           for k,v in ipairs(inputTarget[begin+m-1]) do
+            if pos[m][1] == v then
+                correct = correct + 1
+                break
+            end
+          end 
+        end     
+    end
+    state.bestAccuracy = state.bestAccuracy or 0
+    state.bestEpoch = state.bestEpoch or 0
+    local currAccuracy = correct/(inputDataTensor:size()[1])
+    if currAccuracy > state.bestAccuracy then state.bestAccuracy = currAccuracy; state.bestEpoch = epoch end
+    print(string.format("Epoch %s Accuracy: %s, best Accuracy: %s on epoch %s at time %s", epoch, currAccuracy, state.bestAccuracy, state.bestEpoch, sys.toc() ))
+end
+
+
+
+
+
+
